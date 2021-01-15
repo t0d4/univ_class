8e0498d74213a58ad3177bf1e2dab57299bd9777
t0d4 <todatakaaki@gmail.com>
2021-01-16 00:03:49 +0900

    made output more legible

--- modeling/14/Class/DifferenceMethod.nim
+++ modeling/14/Class/DifferenceMethod.nim
@@ -1,5 +1,7 @@
 import math
 import strformat
+import strutils
+let digits_under_point = 3
 let g = 9.8
 let y0 = 0.0
 let v0 = 0.0
@@ -8,6 +10,7 @@ var y = 0.0
 var y_old = 0.0
 var y_new = 0.0
 var v = 0.0
+
 
 iterator countup*(a: float, b: float, step = 1.0): float {.inline.} =
   var res:float = a
@@ -32,7 +35,7 @@ proc verlet(delta_t: float, y: float, y_old: float): float =
 # Normal analytic solution
 echo "Analytic Solution"
 for t in countup(0.0, 0.40, 0.10):
-    echo fmt"y(t={t})={normal(t)}"
+    echo fmt" (t={t.formatFloat(ffDecimal, digits_under_point)}) y={normal(t).formatFloat(ffDecimal, digits_under_point)}"
 
 
 # Euler method
@@ -40,9 +43,10 @@ echo "Euler method"
 for delta_t in [0.10, 0.050]:
     y = y0
     v = v0
-    echo fmt"delta_t={delta_t}"
+    echo fmt" delta_t={delta_t}"
     for t in countup(0.0, 0.40, delta_t):
-        echo fmt"(t={t}) y={y}, v={v}"
+      if toInt(100*t) mod 10 == 0:  # When t is a multiple of 0.1
+        echo fmt" (t={t.formatFloat(ffDecimal, digits_under_point)}) y={y.formatFloat(ffDecimal, digits_under_point)}, v={v.formatFloat(ffDecimal, digits_under_point)}"
         var next = euler(delta_t, y, v)
         y = next[0]
         v = next[1]
@@ -52,12 +56,12 @@ for delta_t in [0.10, 0.050]:
 echo "Verlet method"
 
 delta_t = 0.10
-echo fmt"(t={0.0}) y={y0}"
+echo fmt" (t={0.0}) y={y0}"
 y_old = y0
 let y1 = y0 + delta_t * v0 - (g*pow(delta_t, 2))/2
 y = y1
 for t in countup(0.10, 0.40, 0.10):
-    echo fmt"(t={t}) y={y}"
+    echo fmt" (t={t.formatFloat(ffDecimal, digits_under_point)}) y={y.formatFloat(ffDecimal, digits_under_point)}"
     y_new = verlet(delta_t, y, y_old)
     y_old = y
     y = y_new

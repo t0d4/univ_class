600a087dc22b65de05367aaaadb7590dcad47b6f
t0d4 <todatakaaki@gmail.com>
2021-01-22 23:38:20 +0900

    Refactored using numpy

--- Cpro/15/coordinate.h
+++
@@ -1,2 +0,0 @@
-double xcoordinate(double v, double theta, double t);
-double ycoordinate(double v, double theta, double t);
--- Cpro/15/illustrate_graph.py
+++ Cpro/15/illustrate_graph.py
@@ -1,13 +1,18 @@
 """
 Requirements:
- - numpy
- - matplotlib
- - Pillow
+    - numpy
+    - matplotlib
+    - Pillow
+Usage:
+    python illustrate_graph.py [csv filenames separated by space]
+Options:
+    --export-png: export graph as a png file
+    --export-gif: export animation as a gif file
+    --remove-trace: remove trace in animation. Must be used with --export-gif
 """
 import os
 import sys
 import argparse
-import csv
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -28,10 +33,18 @@ COLORS = matplotlib.colors.TABLEAU_COLORS.items()
 """
 
 
-def plot_animation_updater(i: int, datalist: list, color_codes: list):
+def init():
+    pass
 
-    plt.xlim(0, 100)
-    plt.ylim(0, 10)
+
+def plot_animation_updater(i: int, datalist: list, color_codes: list, limits: list, remove_trace: bool = False):
+
+    if remove_trace is True:
+        plt.cla()
+
+    print(i, datalist[0]["time_and_coordinates"][i])
+    plt.xlim(0, limits[0]*1.3)
+    plt.ylim(0, limits[1]*1.1)
     for data, color in zip(datalist, color_codes):
         try:
             moment = data["time_and_coordinates"][i]
@@ -50,31 +63,28 @@ def main(files: list, config: dict):
 
     datalist = []
     for file in files:
-        data = {"filename": file, "time_and_coordinates": []}
+        data = {"filename": file, "time_and_coordinates": np.array([])}
 
         if not os.path.exists(file):
             print(f"{file} was not found.")
             sys.exit(1)
 
-        with open(file) as f:
-            reader = csv.reader(f)
-            for row in reader:
-                # Convert each element from str to float
-                data["time_and_coordinates"].append([float(num) for num in row])
+        data["time_and_coordinates"] = np.loadtxt(file, delimiter=',')
+
         datalist.append(data)
+
+    # Maximum of x, y among all data
+    x_max = max([data["time_and_coordinates"].T[1].max() for data in datalist])
+    y_max = max([data["time_and_coordinates"].T[2].max() for data in datalist])
 
     if config["export_png"] is True:
         static_graph = plt.figure()
         axis = static_graph.add_subplot(111, xlabel="x-coordinate", ylabel="y-coordinate")
         for data, (_, color_code) in zip(datalist, COLORS):
-            for idx, moment in enumerate(data["time_and_coordinates"]):
-                # When the first point is plotted, label name is set
-                if idx == 0:
-                    axis.scatter(moment[1], moment[2], color=color_code, label=data["filename"])
-                else:
-                    axis.scatter(moment[1], moment[2], color=color_code)
-        _, y_max = axis.get_ylim()
-        axis.set_ylim(0, y_max)
+            x = data["time_and_coordinates"].T[1]
+            y = data["time_and_coordinates"].T[2]
+            axis.scatter(x, y, color=color_code, label=data["filename"])
+        axis.set_ylim(0, y_max*1.1)
         plt.legend()
         plt.savefig("graph.png")
 
@@ -87,13 +97,17 @@ def main(files: list, config: dict):
     color_codes = color_codes[0:len(datalist)]
     # Fetch the number of frames of the longest throw
     animation_frames = max([len(data["time_and_coordinates"]) for data in datalist])
-    ani = anm.FuncAnimation(animation_figure, plot_animation_updater, fargs=(datalist, color_codes),
-                            interval=100, frames=animation_frames)
+    ani = anm.FuncAnimation(
+                                animation_figure,
+                                plot_animation_updater,
+                                fargs=(datalist, color_codes, [x_max, y_max], config["remove_trace"]),
+                                init_func=init,  # Do nothing
+                                interval=100,
+                                frames=animation_frames
+                            )
 
     if config["export_gif"] is True:
         ani.save("animation.gif")
-
-    #plt.show()
 
 
 if __name__ == '__main__':
@@ -101,8 +115,13 @@ if __name__ == '__main__':
     parser.add_argument("files", nargs="+", help="csv file names, separated by spaces")
     parser.add_argument("--export-png", help="Export graph as a png file", action="store_true")
     parser.add_argument("--export-gif", help="Export animation as a gif file", action="store_true")
+    parser.add_argument("--remove-trace", help="Not display the trace", action="store_true")
     args = parser.parse_args()
-    config = {"export_png": args.export_png, "export_gif": args.export_gif}
+    config = {
+                "export_png": args.export_png,
+                "export_gif": args.export_gif,
+                "remove_trace": args.remove_trace
+             }
 
     if len(args.files) > len(COLORS):
         print(f"This program can't handle more than {len(COLORS)} files.")
